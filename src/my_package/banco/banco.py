a3f90a95ecb8475e0d6ae547dcb099c219e7c2bd
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2026-06-22 19:52:50 -0300

    up to debug

--- src/my_package/banco/banco.py
+++ src/my_package/banco/banco.py
@@ -1,10 +1,27 @@
 import conta
 import person
 
-"""
-deve:
-    - checar a agencia, o usuário, a conta e se a conta É do usário;
-    - deve receber: conta, agencia, cliente.
-    - deve ter um método autenticar q checa.
+"""# class Banco:
+#     def __init__(
+#             self,
+#             agencias:list[int]|None=None,
+#             clientes:list[person.Person]|None=None,
+#             contas:list[int]|None=None,
+#     )"""
 
-"""
+
+if __name__ == "__main__":
+    c1 = person.Client("Matheus", 24)
+    cc1 = conta.ContaCorrente(123, 321, 320, 600)
+    c1.conta = cc1
+    c2 = person.Client("João", 19)
+    cp1 = conta.ContaPoupanca(123, 322, 320)
+    c2.conta = cp1
+    print(c1)
+    print(cc1)
+    print(cp1)
+    cc1.pagar(320)
+    cp1.pagar(318)
+    print(cc1)
+    print(c2)
+    print(cp1)
--- src/my_package/banco/conta.py
+++ src/my_package/banco/conta.py
@@ -2,79 +2,94 @@ from abc import ABC, abstractmethod
 
 
 class Conta(ABC):
-    def __init__(self, agencia:int, conta:int, saldo:float=0):
-        self.agencia = agencia
-        self.conta = conta
-        self.saldo = saldo
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
+        self.agencia: int = agencia
+        self.conta: int = conta
+        self.saldo: float = saldo
 
     @abstractmethod
-    def sacar(self, valor:float)->bool:...
+    def sacar(self, valor: float) -> bool: ...
 
-    def depositar(self, valor:float)->float|None:
-        if valor<=0:
+    @abstractmethod
+    def pagar(self, valor: float) -> None: ...
+    def depositar(self, valor: float) -> float | None:
+        if valor <= 0:
             print(f"O valor não pode ser {valor}")
-        self.saldo+=valor
+        self.saldo += valor
         return self.saldo
-    
-    def to_dict(self)->dict[str, str|int|float]:
+
+    def to_dict(self) -> dict[str, str | int | float]:
         return {
-            'Tipo': self.__class__.__name__,
-            'Agência': self.agencia,
-            'Conta': self.conta,
-            'Saldo': self.saldo,
+            "Tipo": self.__class__.__name__,
+            "Agência": self.agencia,
+            "Conta": self.conta,
+            "Saldo": self.saldo,
         }
-    def __repr__(self):
+
+    def __repr__(self) -> str:
         class_name = self.__class__.__name__
-        attrs = f'agencia={self.agencia}, conta={self.conta}, \
-           saldo={self.saldo}'
-        return f'{class_name}({attrs})'
-    
+        attrs = f"agencia={self.agencia}, conta={self.conta}, \
+           saldo={self.saldo}"
+        return f"{class_name}({attrs})"
+
 
 class ContaPoupanca(Conta):
-    def sacar(self, valor:float)->bool:
+    def sacar(self, valor: float) -> bool:
         valor_pos_saque = self.saldo - valor
-        if valor_pos_saque>=0:
-            self.saldo-=valor
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
             return True
+        print("Valor informado supera o saldo")
         return False
 
+    def pagar(self, valor: float) -> None:
+        valor += 2
+        self.sacar(valor)
+
+
 class ContaCorrente(Conta):
-    def __init__(self, agencia:int, conta:int, saldo:float = 0, limite:float =0):
+    def __init__(
+        self, agencia: int, conta: int, saldo: float = 0, limite: float = 0
+    ) -> None:
         super().__init__(agencia, conta, saldo)
         self.limite = limite
         self.limite_definido = limite
 
-    def sacar(self, valor:float)->bool:
-       new_saldo = self.saldo+self.limite
-       valor_pos_sac = self.saldo - valor
-       if valor>self.saldo and valor<=new_saldo:
-           self.saldo = 0
-           self.limite -= self.saldo-valor
-           return True
-       elif valor_pos_sac>=0:
-           self.saldo-=valor
-           return True
-       else:
-           return False
-       
-    def depositar(self, valor:float)->float|None:
+    def sacar(self, valor: float) -> bool:
+        new_saldo = self.saldo + self.limite
+        valor_pos_sac = self.saldo - valor
+        if valor > self.saldo and valor <= new_saldo:
+            self.saldo = 0
+            self.limite -= valor - self.saldo
+            return True
+        if valor_pos_sac >= 0:
+            self.saldo -= valor
+            return True
+        print("Valor informado supera o saldo")
+        return False
+
+    def pagar(self, valor: float) -> None:
+        valor += 2
+        self.sacar(valor)
+        return
+
+    def depositar(self, valor: float) -> float | None:
         super().depositar(valor)
         if self.limite < self.limite_definido:
-            devido:float = self.limite - self.limite_definido
-            valor-=devido
-            if valor>0:
-                return self.saldo+valor
-            else:
-                print("Valor utilizado para cobrir o cheque especial")
+            devido: float = self.limite - self.limite_definido
+            valor -= devido
+            if valor > 0:
+                return self.saldo + valor
+            print("Valor utilizado para cobrir o cheque especial")
+        return None
 
-       
-    def to_dict(self) -> dict[str, int|str|float]:
+    def to_dict(self) -> dict[str, int | str | float]:
         dict_conta = super().to_dict()
-        dict_conta.update({'Limite': self.limite})
+        dict_conta.update({"Limite": self.limite})
         return dict_conta
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         class_name = self.__class__.__name__
-        attrs = f'agencia={self.agencia}, conta={self.conta}, \
-            saldo={self.saldo}, limite={self.limite}'
-        return f'{class_name}({attrs})'
+        attrs = f"agencia={self.agencia}, conta={self.conta}, \
+            saldo={self.saldo}, limite={self.limite}"
+        return f"{class_name}({attrs})"
